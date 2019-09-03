c303732322b07b466ad52cc0561624e971f9ebdc
sourabh008 <you@example.com>
2019-09-03 18:09:21 +0530

    Updates

--- custom-sw.js
+++ custom-sw.js
@@ -1,5 +1,6 @@
-importScripts("/precache-manifest.8ead1345e948345d45ae093ae0795fc2.js", "/workbox-v4.3.1/workbox-sw.js");
-workbox.setConfig({modulePathPrefix: "/workbox-v4.3.1"});
+importScripts("/precache-manifest.8ead1345e948345d45ae093ae0795fc2.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+
 // See https://developers.google.com/web/tools/workbox/guides/configure-workbox
 workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug);
 
